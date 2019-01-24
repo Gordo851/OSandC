74a011992238ca308abb443c9340cb55a472ccc0
Gordon Rennie <46560768+Gordo851@users.noreply.github.com>
2019-01-24 19:35:35 +0000

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "stack.h"
+
+static int mockUp = 42;
+static int *stackTop = &mockUp;
+
+int *newStack = NULL;
+int *topOfStack = 0;
+int currentSize;
+int stackSize;
+
+void setStackSize( int size)
+{
+    if (newStack == NULL) {
+    newStack = (int*)malloc(size*sizeof(int));
+    stackTop = newStack;
+    stackSize = size;
+    }
+    newStack = (int*)realloc(newStack, (size*sizeof(int)));
+    stackTop = newStack;
+    stackSize = size;
+}
+
+int top()
+{
+   return *stackTop;
+}
+
+int pop( int *val)
+{
+    *val = stackTop;
+    stackTop = stackTop - 1;
+  return 1;
+}
+int push(int val)
+{
+
+    if(newStack == 0) {
+        setStackSize(DEFAULT_STACK_SIZE);
+        currentSize = -1;
+    }
+    if (currentSize == stackSize) {
+        printf("%p\n", stackTop);
+        return 0;
+    }
+    {
+    stackTop = stackTop + 1;
+    printf("%p\n", stackTop);
+    *stackTop = val;
+    currentSize++;
+    return 1;
+}
+}
+
+int getStackSize()
+{
+    return (stackSize);
+}
+
+void deleteStack()
+{
+    free(newStack);
+
+    }
+
---
+++ stack.h
@@ -0,0 +1,6 @@
+#define DEFAULT_STACK_SIZE 10
+
+extern void setStackSize( int size);
+extern int getStackSize();
+extern void deleteStack();
+extern int top();
