64fa44bbc344907d6bea80f11a12e8b711a8279a
Gordon Rennie <46560768+Gordo851@users.noreply.github.com>
2019-01-30 12:57:31 +0000

    Add files via upload
    answers to part 2 -  not finished

---
+++ threads.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
+
+
+void *theMessage(void *ID)
+{
+    printf("I am the first thread. My thread ID: %d. My pID: %d. The first message\n\n", (int)pthread_self(), (int)getpid());
+    printf("I am the first thread. My ID: %d. My pID: %d. The second message\n\n", (int)pthread_self(), (int)getpid());
+    return NULL;
+}
+void *theMessageSecondThread(void *ID)
+{
+    printf("I am the second thread. My thread ID: %d. My pID: %d. The first message\n\n", (int)pthread_self(), (int)getpid());
+    printf("I am the second thread. My ID: %d. My pID: %d. The second message\n\n", (int)pthread_self(), (int)getpid());
+    return NULL;
+}
+
+
+
+
+int main()
+{
+    pthread_t thread0;
+    pthread_t thread1;
+    printf("Hello\n");
+    pthread_create(&thread0, NULL, theMessage, (void *)&thread0);
+    pthread_create(&thread1, NULL, theMessageSecondThread, (void *)&thread1);
+    pthread_join(thread0, NULL);
+    pthread_join(thread1, NULL);
+    return 0;
+}
+
