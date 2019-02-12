c8d45183f9db55a6a85041fea63b3095c0923c4a
Gordon Rennie <46560768+Gordo851@users.noreply.github.com>
2019-02-12 19:59:46 +0000

    Made the pipe
    Sorry, just getting to this now. Made it so we're actually using pipes... 
    
    the For Loops were just me making sure it would print properly. Ran them all the way up to 1000 and no scheduling errors to be found.

--- CW2/problem3.c
+++ CW2/problem3.c
@@ -8,35 +8,44 @@
 void Problem3()
 {
     printf("My process number is: %d\n", (int) getpid());
+    int num = 20;
+    int num1 = 20;
+    int count = 0;
+    int count1 = 0;
+    char buf;
     int fd1[2];
+    if(pipe(fd1)==-1)
+    {
+        printf("pipe failed.\n");
+    }
+
     pid_t x = fork();
+
     if (x < 0) //Fork failed
     {
         printf("Fork Failed...\n");
     }
     if (x == 0) //This is the Child.
     {
-        int sig = 0;
-        int message;
         close(fd1[1]);
-        read(fd1[0], message, 1);
-        sig = message;
-        if (sig != 1) {
-            wait(NULL);
+        read(fd1[0], &buf, 1);
+        for(count = 1; count < num; count++) {
+            printf("First Message: I am the child. My pID is:%d \n", getpid());
+            printf("Second Message: I am now complete. My pID is:%d \n", getpid());
         }
-        printf("First Message: I am the child. My pID is:%d \n", getpid());
-        printf("Second Message: I am now complete. My pID is:%d \n", getpid());
-        close(fd1[0]);
+
+        close(fd1[1]);
         exit(0);
     }
     if (x > 0) //This is the parent.
     {
-        printf("First Message: I am the parent. My pID is: %d\n", (int) getpid());
-        printf("Second Message: I am the parent. I am now complete. My pID is: %d\n", (int) getpid());
-        int sig = 1;
         close(fd1[0]);
-        write(fd1[1], sig, 1);
-        close(fd1[1]);
+        for(count1 = 1; count1 < num1; count1++) {
+            printf("First Message: I am the parent. My pID is: %d\n", (int) getpid());
+            printf("Second Message: I am the parent. I am now complete. My pID is: %d\n", (int) getpid());
+        }
+        write(fd1[1], "A", 1);
+        wait(NULL);
         exit(0);
     }
 }
